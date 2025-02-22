c754edf3c29b75f7d1b8ca137765f8b3543bc2c3
Joao <joaohenrique2@hotmail.com>
2025-02-22 00:29:06 -0300

    modula introducao a seguranca

--- script.js
+++ script.js
@@ -4,23 +4,40 @@ const textoContainer = document.getElementById("texto-container");
 
 const exercicios = {
   "Introdução ao Cloud Foundation": {
-"1 - Introdução à computação em nuvem": "Cloud/2.txt",  
-"2 - O que é computação em nuvem?": "Cloud/5.txt",  
-"3 - O que é a Amazon Web Services?": "Cloud/6.txt",  
-"4 - Fundamentos da definição de preço da AWS": "Cloud/7.txt",  
-"5 - Visão geral da infraestrutura da AWS": "Cloud/8.txt",  
-"6 - Modelo de responsabilidade compartilhada": "Cloud/9.txt",  
-"7 - Introdução ao Amazon S3": "Cloud/10.txt",  
-"8 - Introdução ao Amazon EC2": "Cloud/12.txt",  
-"9 - Vantagens da computação em nuvem": "Cloud/208.txt",  
-"10 -Serviços e categorias da AWS": "Cloud/209.txt"
-
+    "1 - Introdução à computação em nuvem": "Cloud/2.txt",
+    "2 - O que é computação em nuvem?": "Cloud/5.txt",
+    "3 - O que é a Amazon Web Services?": "Cloud/6.txt",
+    "4 - Fundamentos da definição de preço da AWS": "Cloud/7.txt",
+    "5 - Visão geral da infraestrutura da AWS": "Cloud/8.txt",
+    "6 - Modelo de responsabilidade compartilhada": "Cloud/9.txt",
+    "7 - Introdução ao Amazon S3": "Cloud/10.txt",
+    "8 - Introdução ao Amazon EC2": "Cloud/12.txt",
+    "9 - Vantagens da computação em nuvem": "Cloud/208.txt",
+    "10 -Serviços e categorias da AWS": "Cloud/209.txt",
   },
   "Introdução à segurança": {
     "1 - Introdução à segurança": "Seguranca/282.txt",
     "2 - Ciclo de vida da segurança: Prevenção": "Seguranca/283.txt",
-    "3 - Reforço da rede": "Seguranca/284.txt"
-  }
+    "3 - Reforço da rede": "Seguranca/284.txt",
+    "4 - Reforço dos sistemas": "Seguranca/285.txt",
+    "5 - Prevenção: Segurança de dados": "Seguranca/286.txt",
+    "6 - Prevenção: PKI": "Seguranca/287.txt",
+    "7 - Prevenção: Gerenciamento de identidades": "Seguranca/288.txt",
+    "8 - Prevenção: AWS IAM": "Seguranca/289.txt",
+    "9 - Ciclo de vida da segurança: Detecção": "Seguranca/290.txt",
+    "10 - AWS CloudTrail": "Seguranca/291.txt",
+    "11 - AWS Config": "Seguranca/292.txt",
+    "12 - Ciclo de vida da segurança: Resposta": "Seguranca/293.txt",
+    "13 - Ciclo de vida da segurança: Análise": "Seguranca/294.txt",
+    "14 - Trusted Advisor": "Seguranca/295.txt",
+    "15 - Práticas recomendadas de segurança para a criação de conta":
+      "Seguranca/296.txt",
+    "16 - Programa de conformidade de segurança da AWS": "Seguranca/297.txt",
+    "17 - Recursos de segurança da AWS": "Seguranca/298.txt",
+  },
+
+ 
+  },
 };
 
 // Função para carregar a lista de exercícios
@@ -29,7 +46,6 @@ function carregarConteudo(topico) {
   textoContainer.innerHTML = ""; // Limpar o conteúdo do container de texto
   textoContainer.innerHTML = topico; // Atualizar o título
   tituloExercicios.textContent = topico; // Atualizar o título
-  
 
   const listaTopico = exercicios[topico];
 
@@ -47,16 +63,16 @@ function carregarConteudo(topico) {
 // Função para carregar arquivos .txt
 function carregarConteudoExterno(arquivo) {
   fetch(arquivo)
-    .then(response => {
+    .then((response) => {
       if (!response.ok) {
         throw new Error("Erro ao carregar o arquivo.");
       }
       return response.text();
     })
-    .then(texto => {
+    .then((texto) => {
       textoContainer.textContent = texto;
     })
-    .catch(error => {
+    .catch((error) => {
       textoContainer.textContent = "Erro ao carregar o conteúdo.";
       console.error(error);
     });
@@ -64,5 +80,5 @@ function carregarConteudoExterno(arquivo) {
 
 // Chamamos a função automaticamente ao carregar a página
 window.onload = () => {
-  carregarConteudo("Introdução ao Cloud Foundation");// Define o primeiro tópico como padrão
+  carregarConteudo("Introdução ao Cloud Foundation"); // Define o primeiro tópico como padrão
 };
